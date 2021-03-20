746250fa1b90054cab2013307fe5c08c1e970a12
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2021-03-20 10:50:09 +0800

    add central learner & child agent

---
+++ executor/multi_agent_central_learner/child_agent.py
@@ -0,0 +1,41 @@
+import datetime
+import time
+
+class Executor():
+    def __init__(self, agent, n_ppo_update, n_aux_update, runner, save_weights = False, n_saved = 10, load_weights = False, is_training_mode = True):
+        self.agent              = agent
+        self.runner             = runner
+
+        self.n_iteration        = n_iteration
+        self.n_aux_iteration    = n_aux_iteration
+        self.save_weights       = save_weights
+        self.n_saved            = n_saved
+        self.is_training_mode   = is_training_mode
+
+        if load_weights:
+            self.agent.load_weights()
+            print('Weight Loaded')  
+
+    def execute(self):
+        start = time.time()
+        print('Running the training!!')
+
+        try:
+            for i_iteration in range(1, self.n_iteration * self.n_aux_iteration, 1):
+                memories  = self.runner.run()
+                self.agent.save_memory(memories)
+
+                if self.is_training_mode:
+                    self.agent.update()
+
+                    if self.save_weights:
+                        if i_iteration % self.n_saved == 0:
+                            self.agent.save_weights()
+                            print('weights saved')
+
+        except KeyboardInterrupt:
+            print('Stopped by User')
+        finally:
+            finish = time.time()
+            timedelta = finish - start
+            print('\nTimelength: {}'.format(str( datetime.timedelta(seconds = timedelta) )))
