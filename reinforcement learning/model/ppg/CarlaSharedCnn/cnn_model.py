9437e5137119ad7d885b0f9995d12318abe77873
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2021-04-18 20:45:44 +0800

    change cnn model

--- reinforcement learning/model/ppg/CarlaSharedCnn/cnn_model.py
+++ reinforcement learning/model/ppg/CarlaSharedCnn/cnn_model.py
@@ -71,7 +71,7 @@ class CnnModel(nn.Module):
       self.conv_out = nn.Sequential(
         DepthwiseSeparableConv2d(64, 128, kernel_size = 3, stride = 1, padding = 1),
         nn.ReLU(),
-        DepthwiseSeparableConv2d(128, 128, kernel_size = 3, stride = 1, padding = 1),
+        DepthwiseSeparableConv2d(128, 128, kernel_size = 5, stride = 1, padding = 0),
         nn.ReLU(),
       )
         
